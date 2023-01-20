013a68a8c73b37d34fa0858b60bb3e0e296e3479
Olity <olity1712@gmail.com>
2023-01-20 17:27:22 +0600

    seed DB

--- backend/db/seeders/6-Members.js
+++ backend/db/seeders/6-Members.js
@@ -120,6 +120,120 @@ module.exports = {
                 updatedAt: new Date()
             },
             {
+                user_id: 6,
+                event_id: 1,
+                createdAt: new Date(),
+                updatedAt: new Date()
+            },
+            {
+                user_id: 6,
+                event_id: 2,
+                createdAt: new Date(),
+                updatedAt: new Date()
+            },
+            {
+                user_id: 6,
+                event_id: 3,
+                createdAt: new Date(),
+                updatedAt: new Date()
+            },
+            {
+                user_id: 6,
+                event_id: 4,
+                createdAt: new Date(),
+                updatedAt: new Date()
+            },
+            {
+                user_id: 6,
+                event_id: 5,
+                createdAt: new Date(),
+                updatedAt: new Date()
+            },
+            {
+                user_id: 6,
+                event_id: 6,
+                createdAt: new Date(),
+                updatedAt: new Date()
+            },
+            {
+                user_id: 6,
+                event_id: 7,
+                createdAt: new Date(),
+                updatedAt: new Date()
+            },
+            {
+                user_id: 6,
+                event_id: 8,
+                createdAt: new Date(),
+                updatedAt: new Date()
+            },
+            {
+                user_id: 6,
+                event_id: 9,
+                createdAt: new Date(),
+                updatedAt: new Date()
+            },
+            {
+                user_id: 6,
+                event_id: 10,
+                createdAt: new Date(),
+                updatedAt: new Date()
+            },
+            {
+                user_id: 6,
+                event_id: 11,
+                createdAt: new Date(),
+                updatedAt: new Date()
+            },
+            {
+                user_id: 6,
+                event_id: 12,
+                createdAt: new Date(),
+                updatedAt: new Date()
+            },
+            {
+                user_id: 6,
+                event_id: 13,
+                createdAt: new Date(),
+                updatedAt: new Date()
+            },
+            {
+                user_id: 6,
+                event_id:14,
+                createdAt: new Date(),
+                updatedAt: new Date()
+            },
+            {
+                user_id: 6,
+                event_id: 15,
+                createdAt: new Date(),
+                updatedAt: new Date()
+            },
+            {
+                user_id: 6,
+                event_id: 16,
+                createdAt: new Date(),
+                updatedAt: new Date()
+            },
+            {
+                user_id: 6,
+                event_id: 17,
+                createdAt: new Date(),
+                updatedAt: new Date()
+            },
+            {
+                user_id: 6,
+                event_id: 18,
+                createdAt: new Date(),
+                updatedAt: new Date()
+            },
+            {
+                user_id: 6,
+                event_id: 19,
+                createdAt: new Date(),
+                updatedAt: new Date()
+            },
+            {
                 user_id: 7,
                 event_id: 20,
                 createdAt: new Date(),
